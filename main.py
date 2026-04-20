46964bdbacf8ba394964b554e2f3c0d67f8cac8e
Yanzhe Li <YLi703@sheffield.ac.uk>
2026-04-20 22:17:56 +0100

    Apply week 8 refactoring improvements and add mini report

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class CustomerManager:
         self.discount_threshold = 500
 
     def add_customer(self, name, purchases):
-        if name in self.customers.keys():
+        if name in self.customers:
             self.customers[name].extend(purchases)
         else:
             self.customers[name] = purchases
@@ -18,54 +18,69 @@ class CustomerManager:
         self.add_customer(name, purchases)
 
     def generate_report(self):
-        for y, x in self.customers.items():
-            a = 0
-            for z in x:
-                if z['price'] > self.tax_threshold:
-                    taxed_price = z['price'] * (1 + self.tax_rate)
-                    a += taxed_price
-                else:
-                    a += z['price']
-            print(y)
-            if a > self.discount_threshold:
-                print("Eligible for discount")
-            else:
-                if a > 300:
-                    print("Potential future discount customer")
-                else:
-                    print("No discount")
-            if a > 1000:
-                print("VIP Customer!")
-            else:
-                if a > 800:
-                    print("Priority Customer")
+        for customer_name, purchases in self.customers.items():
+            total_spent = self._calculate_total_spent(purchases)
+            print(customer_name)
+            self._print_discount_status(total_spent)
+            self._print_priority_status(total_spent)
+
+    def _calculate_total_spent(self, purchases):
+        total_spent = 0
+        for purchase in purchases:
+            total_spent += self._apply_tax_if_needed(purchase["price"])
+        return total_spent
+
+    def _apply_tax_if_needed(self, price):
+        if price > self.tax_threshold:
+            return price * (1 + self.tax_rate)
+        return price
+
+    def _print_discount_status(self, total_spent):
+        if total_spent > self.discount_threshold:
+            print("Eligible for discount")
+        elif total_spent > 300:
+            print("Potential future discount customer")
+        else:
+            print("No discount")
+
+    def _print_priority_status(self, total_spent):
+        if total_spent > 1000:
+            print("VIP Customer!")
+        elif total_spent > 800:
+            print("Priority Customer")
 
     def calculate_shipping_fee(self, purchases):
-        heavy_item = False
-        for purchase in purchases:
-            if purchase.get('weight', 0) > 20:
-                heavy_item = True
-                break
-        if heavy_item:
-            return 50
-        else:
-            return 20
+        return _calculate_shipping_fee_by_rule(
+            purchases,
+            lambda purchase: purchase.get("weight", 0) > 20,
+            50,
+            20,
+        )
+
+
+def _calculate_shipping_fee_by_rule(purchases, condition, matched_fee, default_fee):
+    for purchase in purchases:
+        if condition(purchase):
+            return matched_fee
+    return default_fee
+
 
 def calculate_shipping_fee_for_heavy_items(purchases):
-    for purchase in purchases:
-        if purchase.get('weight', 0) > 20:
-            return 50
-    return 20
+    return _calculate_shipping_fee_by_rule(
+        purchases,
+        lambda purchase: purchase.get("weight", 0) > 20,
+        50,
+        20,
+    )
+
 
 def calculate_shipping_fee_for_fragile_items(purchases):
-    fragile_item = False
-    for purchase in purchases:
-        if purchase.get('fragile', False):
-            fragile_item = True
-            break
-    if fragile_item:
-        return 60
-    else:
-        return 25
+    return _calculate_shipping_fee_by_rule(
+        purchases,
+        lambda purchase: purchase.get("fragile", False),
+        60,
+        25,
+    )
+
 
 flat_tax = 0.2
